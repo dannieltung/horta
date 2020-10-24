f5235f268ab7a506738898d7d9fe7bc107fed949
Danniel Tung <dwftung@gmail.com>
2020-10-24 17:04:11 -0300

    Validation, Routes and Controllers done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :transactions
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :stock, presence: true
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :product
   belongs_to :user
+  validates :quantity, presence: true
+  validates :status, default: false
 end
