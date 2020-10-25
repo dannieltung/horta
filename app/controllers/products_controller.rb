8043fd9ef5d106225579a4fc231cf2ab55d3eaf1
Danniel Tung <dwftung@gmail.com>
2020-10-25 13:55:43 -0300

    product's controller and views done. user_id#1 createad and seeded with products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,49 @@
 class ProductsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @product = Product.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @product = Product.new(product_params)
+    # aqui ele vai instanciar um product com caracteristicas NEW
+    @product.user = @user
+    if @product.save
+      redirect_to product_path(@product), notice: 'Product created!'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    # aqui ele vai instanciar com as características do product achado
+    if @product.update(product_params)
+      redirect_to product_path(@product), notice: 'Product updated!'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :stock, :price, :user_id)
+  end
+  # é certo eu liberar o user_id em caso de update? user pode dizer que pertence a outro user.
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index] do
     resources :transactions, only: [:new, :create, :show]
-    resources :products, except: [:destroy]
+    resources :products, only: [:index, :new, :create]
   end
+  resources :products, only: [:edit, :update, :show]
 end
