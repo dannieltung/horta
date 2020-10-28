db07e081356430cafffc6c79bf1a8299c3dd6f34
Eduardo Pini <appiopini@gmail.com>
2020-10-28 17:35:35 -0300

    Updated Routes and Current_User and Migrate

--- db/migrate/20201028010217_add_status_default_false.rb
+++ db/migrate/20201028010217_add_status_default_false.rb
@@ -1,5 +1,5 @@
 class AddStatusDefaultFalse < ActiveRecord::Migration[6.0]
   def change
-    change
+    change_column :transactions, :status, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_25_220748) do
+ActiveRecord::Schema.define(version: 2020_10_28_010217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2020_10_25_220748) do
 
   create_table "transactions", force: :cascade do |t|
     t.integer "quantity"
-    t.boolean "status"
+    t.boolean "status", default: false
     t.bigint "product_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
