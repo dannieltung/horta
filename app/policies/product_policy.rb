7dea01a5bc0ab6e2f29805b84007dee0c6517ed5
Danniel Tung <dwftung@gmail.com>
2020-10-29 00:57:43 -0300

    authorization done with pundit and if current_user

--- app/policies/product_policy.rb
+++ app/policies/product_policy.rb
@@ -2,29 +2,41 @@ class ProductPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       scope.where(user: user)
+      # scope ==> Product (a classe que voce esta passando)
+      # aqui é o scope padrão e no controller é a continuação da query.
     end
-  end
-
-  def new?
-    true
   end
 
   def show?
     true
   end
 
-  def index?
+  def new?
+    true
+  end
+
+  def create?
     true
   end
 
   def edit?
-    # user   ==> current_user
-    # record ==> @product (objeto que vc esta passando)
-    if record.user == user
-      true
-    else
-      false
-    end
+    owner?
   end
 
+  def update?
+    owner?
+  end
+
+  def destroy?
+    owner?
+  end
+
+  private
+
+  def owner?
+    # record.user == user || user.admin
+    record.user == user
+    # user   ==> current_user
+    # record ==> @product (objeto que vc esta passando)
+  end
 end
