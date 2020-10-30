68be8d647b5ffcaa7505cc0961a58a4bc37e929d
Danniel Tung <dwftung@gmail.com>
2020-10-30 19:11:14 -0300

    added latitude e longitude columns to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_220353) do
+ActiveRecord::Schema.define(version: 2020_10_30_221029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2020_10_30_220353) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "remove", default: false
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
