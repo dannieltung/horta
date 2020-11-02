3f6876a590c2636fd2dd9e87babb2485fab899f4
Danniel Tung <dwftung@gmail.com>
2020-11-01 21:17:46 -0300

    solved delivery address on user new registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    # @user = current_user
     unless @user == current_user
       redirect_to root_path, notice: 'Not your account! 😠'
     end
