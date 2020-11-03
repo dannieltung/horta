0d8da35edfdb9b6f65d08e67445426cfcc655dd2
Eduardo Pini <appiopini@gmail.com>
2020-11-03 00:23:24 -0300

    Applied Font Awesome

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+<svg class="bi" fill="currentColor">
+  <use xlink:href="<%= asset_path "bootstrap-icons/bootstrap-icons.svg" %>#shop"/>
+</svg>
